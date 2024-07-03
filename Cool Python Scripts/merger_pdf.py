ea9e89636bb3c264c70d3eb947c46ad5c823b4db
Walter <walterarbiol@gmail.com>
2024-07-03 12:35:57 -0300

    update

--- Cool Python Scripts/merger_pdf.py
+++ Cool Python Scripts/merger_pdf.py
@@ -1,24 +1,26 @@
 import PyPDF2
 
-# import glob
-# pdfs = sorted(glob.glob('./*.pdf'))
+import glob
+pdfs = sorted(glob.glob('./*.pdf'))
 
-pdfs = ['pdf1.pdf', 
-	'pdf2.pdf',
-        'pdf3.pdf']
+# pdfs = ['pdf1.pdf', 
+# 	'pdf2.pdf',
+#         'pdf3.pdf']
 
 writer = PyPDF2.PdfWriter()
 for pdf in pdfs:
     reader = PyPDF2.PdfReader(pdf)
     pages = reader.pages
     for i, page in enumerate(pages):
-        # if pdf != "Q1.pdf" or i != 1:
+        # if pdf != "./Q1.pdf" or i != 1:
         #     page.scale_to(width=595, height=842)
         # else:
         #     page.scale_to(width=595, height=120)
+        if pdf == "./Q1-4.pdf" or pdf ==  "./Qz10.pdf":
+            page.scale_by(0.4)
         writer.add_page(page)
 
-
-output = open('seu_output.pdf', 'wb')
+output = open('E1-Walter_Melhado_Arbiol_Forne.pdf', 'wb')
 writer.write(output)
 output.close()
+
